63b2f21492ea763697a7f202b2e083ed2923d61f
cf-11 <montesclaros.vince@gmail.com>
2015-03-23 14:06:26 +0800

    Improved login and added the redirect after successful login feature -- redirects to the wall page where posts are displayed.

--- FacebookWall/src/FacebookWall/posts/views.py
+++ FacebookWall/src/FacebookWall/posts/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
 from django.views.generic import ListView, CreateView
 from posts.models import Post
+from django.http import HttpResponseRedirect
+
+
+def logout_page(request):
+    logout(request)
+    # Redirect to a success page.
+    # return render(request, 'login.html')
+    return HttpResponseRedirect('/')
+
+
+def index(request):
+    # pass posts here
+    return render(request, 'feed.html')
 
 
 class ListPostView(ListView):
     model = Post
-    template_name = 'posts_list.html'
+    template_name = 'feed.html'
 
 
 class CreatePostView(CreateView):
@@ -15,4 +27,4 @@ class CreatePostView(CreateView):
     template_name = 'edit_post.html'
 
     def get_success_url(self):
-        return reverse('posts_list')
+        return reverse('feed')
